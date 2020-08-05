8f231256a6f7e1c00598e2c3daad0952e71b2641
KR1$#N@ <krishshah920@gmail.com>
2020-08-05 21:43:14 +0530

    UpDated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
 
 app.set('view engine', 'ejs')
-app.use(express('public'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`)
